9d7ab8f76305921c810bbf473168f6ba086e0b61
Anwesh43 <anweshthecool0@gmail.com>
2020-02-12 10:50:04 +0530

    adding Canvas extension methods

--- sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
+++ sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
@@ -20,8 +20,46 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val lSizeFactor : Float = 3f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSqRotBouncy(i : Int, scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, lines)
+    val lSize : Float = size / lSizeFactor
+    save()
+    rotate(rot * sf)
+    save()
+    translate(size, -size)
+    for (j in 0..1) {
+        save()
+        rotate(rot * j)
+        drawLine(0f, 0f, 0f, lSize, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawCornerRotSqs(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawSqRotBouncy(j, scale, size, paint)
+    }
+}
+
+fun Canvas.drawSCRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawCornerRotSqs(scale, size, paint)
+    restore()
+}
