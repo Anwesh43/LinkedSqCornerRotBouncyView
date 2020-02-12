e130bc87948fd78fd0b5845876f04bdeeacac20b
Anwesh43 <anweshthecool0@gmail.com>
2020-02-12 11:00:45 +0530

    adding SqCornerRotBouncy class

--- sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
+++ sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
@@ -172,4 +172,28 @@ class SqCornerRotBouncyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SqCornerRotBouncy(var i : Int) {
+
+        private val root : SCRBNode = SCRBNode(0)
+        private var curr : SCRBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
