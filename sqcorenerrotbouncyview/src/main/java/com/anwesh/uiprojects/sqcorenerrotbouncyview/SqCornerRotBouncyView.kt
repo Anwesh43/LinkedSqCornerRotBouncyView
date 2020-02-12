7f71fa87c0d79ec35711d513e12d4636d7746adb
Anwesh43 <anweshthecool0@gmail.com>
2020-02-12 10:58:12 +0530

    adding SCRBNode class

--- sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
+++ sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
@@ -130,4 +130,46 @@ class SqCornerRotBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCRBNode(var i : Int, val state : State = State()) {
+
+        private var next : SCRBNode? = null
+        private var prev : SCRBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SCRBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCRBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCRBNode {
+            var curr : SCRBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
