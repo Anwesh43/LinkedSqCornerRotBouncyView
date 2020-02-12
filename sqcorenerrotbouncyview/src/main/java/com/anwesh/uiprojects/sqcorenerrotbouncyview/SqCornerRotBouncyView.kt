47dd85d60e026d48f49acb90f9e5b5830cf2c65e
Anwesh43 <anweshthecool0@gmail.com>
2020-02-12 10:51:47 +0530

    adding SqCornerRotBouncyView class

--- sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
+++ sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
@@ -63,3 +63,21 @@ fun Canvas.drawSCRBNode(i : Int, scale : Float, paint : Paint) {
     drawCornerRotSqs(scale, size, paint)
     restore()
 }
+
+class SqCornerRotBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
