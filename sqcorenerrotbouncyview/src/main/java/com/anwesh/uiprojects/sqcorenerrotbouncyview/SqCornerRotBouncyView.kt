64a3fffbca8d03575f6b4977300c0bb8f7fbdf14
Anwesh43 <anweshthecool0@gmail.com>
2020-02-12 10:55:33 +0530

    adding Animator clas

--- sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
+++ sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
@@ -21,6 +21,8 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val lSizeFactor : Float = 3f
+val delay : Long = 20
+
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -97,6 +99,34 @@ class SqCornerRotBouncyView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
