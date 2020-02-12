91c5459a9b2f5617a3f2e8ad18b662a370da0d5a
Anwesh43 <anweshthecool0@gmail.com>
2020-02-12 10:41:44 +0530

    adding constants and libraries

--- sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
+++ sqcorenerrotbouncyview/src/main/java/com/anwesh/uiprojects/sqcorenerrotbouncyview/SqCornerRotBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.sqcorenerrotbouncyview
 /**
  * Created by anweshmishra on 12/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val rot : Float = 90f
+val scGap : Float = 0.02f / lines
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
